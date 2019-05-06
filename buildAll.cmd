586f8dffd3e254b7631410ed1891c2a8467660e5
Connor Reviere <creviere12@gmail.com>
2019-05-06 18:28:58 -0500

    Add logging and error handling to build script for Windows

--- buildAll.cmd
+++ buildAll.cmd
@@ -3,7 +3,8 @@
 ::
 :: Author: cr
 
-:: find vcvarsall for VS2017
+setlocal
+
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" (
   set vcvars="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
 )
@@ -11,43 +12,62 @@ if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Au
   set vcvars="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat"
 )
 
+if not defined logfile set logfile=..\build.log
+if exist %logfile% del %logfile%
+
 where cmake >nul 2>nul
 if errorlevel 1 (
   if not exist "C:\Program Files\CMake\bin\cmake.exe" echo CMake must be installed to continue && exit
   set cmakecmd="C:\Program Files\CMake\bin\cmake.exe"
 ) else (set cmakecmd=cmake)
 
-:: build for x86
+echo Building PDF Writer library
+
+if exist build32 (
+  echo Cleaning up leftovers from previous builds
+  @rd /s /q build32
+  if exist build64 @rd /s /q build64
+)
+
+cd "%~dp0"
+
 setlocal
+echo Generating x86 build files
 mkdir build32 && cd build32
-%cmakecmd% -G "Visual Studio 15 2017" -DPDFHUMMUS_NO_AES=true -DUNICODE_CHARSET=true -DMSVC_USE_STATIC_RUNTIME=true ..
+%cmakecmd% -G "Visual Studio 15 2017" -DPDFHUMMUS_NO_AES=true -DUNICODE_CHARSET=true -DMSVC_USE_STATIC_RUNTIME=true .. >> %logfile%
+if errorlevel 1 goto error
 
-::sed -i 's/\/MD/\/MT/g' CMakeCache.txt
 powershell -Command "(gc CMakeCache.txt) -replace '/MD', '/MT' | Out-File -encoding ASCII CMakeCache.txt"
 
-call %vcvars% x86
+call %vcvars% x86 >> %logfile%
 for %%c in (Debug Release) do (
-  msbuild "PDFHUMMUS.sln" /t:Build /v:quiet /p:Configuration=%%c /p:Platform=Win32 /m
+  echo Building library for x86 - %%c
+  msbuild "PDFHUMMUS.sln" -t:Build -p:Configuration=%%c -p:Platform=Win32 /m >> %logfile%
+  if errorlevel 1 goto error
 )
-if errorlevel 1 goto error
 endlocal
 
-:: build for x64
 setlocal
+echo Generating x64 build files
 mkdir build64 && cd build64
-%cmakecmd% -G "Visual Studio 15 2017 Win64" -DPDFHUMMUS_NO_AES=true -DUNICODE_CHARSET=true -DMSVC_USE_STATIC_RUNTIME=true ..
+%cmakecmd% -G "Visual Studio 15 2017 Win64" -DPDFHUMMUS_NO_AES=true -DUNICODE_CHARSET=true -DMSVC_USE_STATIC_RUNTIME=true .. >> %logfile%
+if errorlevel 1 goto error
 
-::sed -i 's/\/MD/\/MT/g' CMakeCache.txt
 powershell -Command "(gc CMakeCache.txt) -replace '/MD', '/MT' | Out-File -encoding ASCII CMakeCache.txt"
 
-call %vcvars% x64
+call %vcvars% x64 >> %logfile%
 for %%c in (Debug Release) do (
-  msbuild "PDFHUMMUS.sln" /t:Build /v:quiet /p:Configuration=%%c /p:Platform=x64 /m
+  echo Building library for x64 - %%c
+  msbuild "PDFHUMMUS.sln" /t:Build /v:quiet /p:Configuration=%%c /p:Platform=x64 /m >> %logfile%
+  if errorlevel 1 goto error
 )
-if errorlevel 1 goto error
 endlocal
 
+echo Finished building PDF Writer library
+
+endlocal
 goto :eof
 
 :error
-  An error occurred, check the previous output to determine what went wrong
+  echo 'PDF-Writer' build failed - check build.log for more details
+  
